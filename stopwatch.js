41e6dc27663975e983c4d0d4bf571e678307a804
Anh Tuan <thongocnguyen1011@gmail.com>
2023-11-03 13:07:23 +0700

    update addRowLap function

--- stopwatch.js
+++ stopwatch.js
@@ -1,16 +1,15 @@
 const timerElement = document.querySelector('.js-timer');
-let IntervalTimer;
-let IntervalLap;
+let Interval;
 
 displayTimer();
 
 function displayTimer() {
   const timerObject = getValue();
-  const i = timerObject.second;
-  const j = timerObject.minute;
-  const z = timerObject.hour;
-  const lap = JSON.parse(localStorage.getItem('lap')) || 0;
-  const displayLapHTML = localStorage.getItem('displayLap') || '';
+  let i = timerObject.second;
+  let j = timerObject.minute;
+  let z = timerObject.hour;
+  let lap = JSON.parse(localStorage.getItem('lap')) || 0;
+  let displayLapHTML = localStorage.getItem('displayLap') || '';
   const displayLapElement = document.querySelector('.js-display-lap');
 
   if (lap > 0) {
@@ -59,6 +58,68 @@ function getValue() {
   return { second, minute, hour };
 }
 
+function startCountUp() {
+  const startStop = document.querySelector('.js-start-stop');
+  const lapElement = document.querySelector('.js-reset-lap');
+  const timerObject = getValue();
+  // let { second, minute, hour } = timerObject;
+  let i = timerObject.second;
+  let j = timerObject.minute;
+  let z = timerObject.hour;
+
+  if (startStop.innerHTML === 'Start') {
+    startStop.innerHTML = 'Stop';
+    lapElement.innerHTML = 'Lap';
+    startStop.classList.add('stop-bg');
+
+    Interval = setInterval(() => {
+      if (i < 9 && j < 10) {
+        if (z > 0)
+          timerElement.innerHTML = `${z}:0${j}:0${++i}`;
+        else
+          timerElement.innerHTML = `0${j}:0${++i}`;
+      } else if (i < 9 && j >= 10) {
+        if (z > 0)
+          timerElement.innerHTML = `${z}:${j}:0${++i}`;
+        else
+          timerElement.innerHTML = `${j}:0${++i}`;
+      } else if (i >= 9 && i < 59 && j < 10) {
+        if (z > 0)
+          timerElement.innerHTML = `${z}:0${j}:${++i}`;
+        else
+          timerElement.innerHTML = `0${j}:${++i}`;
+      } else if (i >= 9 && i < 59 && j >= 10) {
+        if (z > 0)
+          timerElement.innerHTML = `${z}:${j}:${++i}`;
+        else
+          timerElement.innerHTML = `${j}:${++i}`;
+      } else if (i === 59 && j < 9) {
+        i = 0;
+        if (z > 0)
+          timerElement.innerHTML = `${z}:0${++j}:00`;
+        else
+          timerElement.innerHTML = `0${++j}:00`;
+      } else if (i === 59 && j >= 9 && j < 59) {
+        i = 0;
+        if (z > 0)
+          timerElement.innerHTML = `${z}:${++j}:00`;
+        else
+          timerElement.innerHTML = `${++j}:00`;
+      } else {
+        i = 0;
+        j = 0;
+        timerElement.innerHTML = `${++z}:00:00`;
+      }
+      saveToLocalStorage(i, j, z);
+    }, 1000);
+  } else {
+    startStop.innerHTML = 'Start';
+    lapElement.innerHTML = 'Reset';
+    startStop.classList.remove('stop-bg');
+    clearInterval(Interval);
+  }
+}
+
 function addRowLap(displayLapElement, lap, value) {
   displayLapElement.innerHTML = `
     <div class="row-lap">
@@ -73,169 +134,43 @@ function addRowLap(displayLapElement, lap, value) {
   return displayLapElement.innerHTML;
 }
 
-function startCountUp() {
-  const startStop = document.querySelector('.js-start-stop');
-  const lapElement = document.querySelector('.js-reset-lap');
-  const displayLapElement = document.querySelector('.js-display-lap');
-  const timerObject = getValue();
-  // let { second, minute, hour } = timerObject;
-  let i = timerObject.second;
-  let j = timerObject.minute;
-  let z = timerObject.hour;
-
-  if (startStop.innerHTML === 'Start') {
-    startStop.innerHTML = 'Stop';
-    lapElement.innerHTML = 'Lap';
-    startStop.classList.add('stop-bg');
-
-    IntervalTimer = setInterval(() => {
-      const lap = JSON.parse(localStorage.getItem('lap')) || 1;
-
-      if (!displayLapElement.classList.contains('display-lap'))
-        displayLapElement.classList.add('display-lap');
-      if (i < 9 && j < 10) {
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:0${j}:0${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:0${j}:0${i}`);
-        }
-        else {
-          timerElement.innerHTML = `0${j}:0${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `0${j}:0${i}`);
-        }
-      } else if (i < 9 && j >= 10) {
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:${j}:0${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:${j}:0${i}`);
-        }
-        else {
-          timerElement.innerHTML = `${j}:0${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${j}:0${i}`);
-        }
-      } else if (i >= 9 && i < 59 && j < 10) {
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:0${j}:${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:0${j}:${i}`);
-        }
-        else {
-          timerElement.innerHTML = `0${j}:${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `0${j}:${i}`);
-        }
-      } else if (i >= 9 && i < 59 && j >= 10) {
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:${j}:${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:${j}:${i}`);
-        }
-        else {
-          timerElement.innerHTML = `${j}:${++i}`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${j}:${i}`);
-        }
-      } else if (i === 59 && j < 9) {
-        i = 0;
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:0${++j}:00`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:0${j}:00`);
-        }
-        else {
-          timerElement.innerHTML = `0${++j}:00`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `0${j}:00`);
-        }
-      } else if (i === 59 && j >= 9 && j < 59) {
-        i = 0;
-        if (z > 0) {
-          timerElement.innerHTML = `${z}:${++j}:00`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:${j}:00`);
-        }
-        else {
-          timerElement.innerHTML = `${++j}:00`;
-          if (lap === 1)
-            displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${j}:00`);
-        }
-      } else {
-        i = 0;
-        j = 0;
-        timerElement.innerHTML = `${++z}:00:00`;
-        if (lap === 1)
-          displayLapElement.innerHTML = addRowLap(displayLapElement, lap, `${z}:00:00`);
-      }
-      saveToLocalStorage(i, j, z);
-      localStorage.setItem('displayLap', displayLapElement.innerHTML);
-    }, 1000);
-  } else {
-    startStop.innerHTML = 'Start';
-    lapElement.innerHTML = 'Reset';
-    startStop.classList.remove('stop-bg');
-    clearInterval(IntervalTimer);
-    clearInterval(IntervalLap);
-  }
-}
-
 function resetLapTimer() {
   const lapElement = document.querySelector('.js-reset-lap');
   const displayLapElement = document.querySelector('.js-display-lap');
-  let displayLapHTML = localStorage.getItem('displayLap');
-  let i = JSON.parse(localStorage.getItem('timer-second-lap')) || 0;
-  let j = JSON.parse(localStorage.getItem('timer-minute-lap')) || 0;
-  let z = JSON.parse(localStorage.getItem('timer-hour-lap')) || 0;
-  let lap = JSON.parse(localStorage.getItem('lap')) || 1;
-
-  displayLapElement.innerHTML = localStorage.getItem('displayLap');
+  let displayLapHTML = localStorage.getItem('displayLap') || '';
+  const timerObject = getValue();
+  let i = timerObject.second;
+  let j = timerObject.minute;
+  let z = timerObject.hour;
+  let lap = JSON.parse(localStorage.getItem('lap')) || 0;
 
   if (lapElement.innerHTML === 'Lap') {
-    lap++;
-    IntervalLap = setInterval(() => {
-      if (i < 9 && j < 10)
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:0${j}:0${++i}`);
-        else 
-          displayLapHTML += addRowLap(displayLapElement, lap, `0${j}:0${++i}`);
-      else if (i >= 9 && i < 59 && j < 10)
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:0${j}:${++i}`);
-        else
-          displayLapHTML += addRowLap(displayLapElement, lap, `0${j}:${++i}`);
-      else if (i < 9 && j >= 10)
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:${j}:0${++i}`);
-        else
-          displayLapHTML += addRowLap(displayLapElement, lap, `${j}:0${++i}`);
-      else if (i >= 9 && i < 59 && j >= 10)
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:${j}:${++i}`);
-        else
-          displayLapHTML += addRowLap(displayLapElement, lap, `${j}:${++i}`);
-      else if (i === 59 && j < 9) {
-        i = 0;
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:0${++j}:00`);
-        else
-          displayLapHTML += addRowLap(displayLapElement, lap, `0${++j}:00`);
-      } else if (i === 59 && j >= 9 && j < 59) {
-        i = 0;
-        if (z > 0)
-          displayLapHTML += addRowLap(displayLapElement, lap, `${z}:${++j}:00`);
-        else
-          displayLapHTML += addRowLap(displayLapElement, lap, `${++j}:00`);
-      } else {
-        i = 0;
-        j = 0;
-        displayLapHTML += displayLapElement(displayLapElement, lap, `${++z}:00:00`);
-      }
-      saveToLocalStorageLap(i, j, z);
-      displayLapElement.innerHTML = displayLapHTML;
-      localStorage.setItem('displayLap', displayLapHTML);
-      localStorage.setItem('lap', JSON.stringify(lap));
-    }, 1000);
+    if (!displayLapElement.classList.contains('display-lap'))
+      displayLapElement.classList.add('display-lap');
+    if (i < 10 && j < 10) {
+      if (z > 0)
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${z}:0${j}:0${i}`);
+      else
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `0${j}:0${i}`);
+    } else if (i < 10 && j >= 10) {
+      if (z > 0)
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${z}:${j}:0${i}`);
+      else
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${j}:0${i}`);
+    } else if (i >= 10 && j < 10) {
+      if (z > 0)
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${z}:0${j}:${i}`);
+      else
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `0${j}:${i}`);
+    } else {
+      if (z > 0)
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${z}:${j}:${i}`);
+      else
+        displayLapHTML += addRowLap(displayLapElement, ++lap, `${j}:${i}`);
+    }
+    displayLapElement.innerHTML = displayLapHTML;
+    localStorage.setItem('displayLap', displayLapHTML);
+    localStorage.setItem('lap', JSON.stringify(lap));
   } else {
     displayLapElement.classList.remove('display-lap');
     displayLapElement.innerHTML = '';
@@ -252,12 +187,6 @@ function saveToLocalStorage(second, minute, hour) {
   localStorage.setItem('timer-second', JSON.stringify(second));
   localStorage.setItem('timer-minute', JSON.stringify(minute));
   localStorage.setItem('timer-hour', JSON.stringify(hour));
-}
-
-function saveToLocalStorageLap(second, minute, hour) {
-  localStorage.setItem('timer-second-lap', JSON.stringify(second));
-  localStorage.setItem('timer-minute-lap', JSON.stringify(minute));
-  localStorage.setItem('timer-hour-lap', JSON.stringify(hour));
 }
 
 document.querySelector('.js-start-stop')
